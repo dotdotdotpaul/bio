78c96e3dc2bdb65c53e7b207e09ca4c3b5d97fd0
Paul Clegg <dotdotdotpaul@gmail.com>
2016-03-04 06:45:00 -0800

    Remove defstruct for NA sequence (no real need, yet).  Realized my reverse_complement was really just complement, cleaned up and then added actual reverse_complement based on it.

--- lib/bio/sequence/na.ex
+++ lib/bio/sequence/na.ex
@@ -1,31 +1,39 @@
 defmodule Bio.Sequence.NA do
-  defstruct seq: ""
 
-  def reverse_complement(na = %Bio.Sequence.NA{}) do
-    %Bio.Sequence.NA{na | seq: reverse_complement(na.seq)}
-  end
-  def reverse_complement(bases) do
-    bases
+  def complement(bases) when is_bitstring(bases) do
+    String.downcase(bases)
     |> to_char_list
-    |> Enum.map(fn(x) -> reverse_complement_one_base(x) end)
+    |> complement
     |> to_string
   end
-
-  def reverse_complement_one_base(base) when is_integer(base) do
-    reverse_complement_one_base(to_string [base])
+  def complement(bases) when length(bases) > 1 do
+    Enum.map(bases, &(complement(&1)))
   end
-  def reverse_complement_one_base(base) do
+  def complement(base) when is_integer(base) do
+    [c|_] = complement([base])
+    c
+  end
+  def complement(base) do
     #  atgcrymkdhvbswn
     #  tacgyrkmhdbvswn
-    %{ "a" => "t", "t" => "a",
-       "c" => "g", "g" => "c",
-       "r" => "y", "y" => "r",
-       "m" => "k", "k" => "m",
-       "d" => "h", "h" => "d",
-       "v" => "b", "b" => "v",
-       "s" => "s", "w" => "w", "n" => "n",
-       "u" => "a"
-     }[String.downcase(base)] || base
+    %{ 'a' => 't', 't' => 'a',
+       'c' => 'g', 'g' => 'c',
+       'r' => 'y', 'y' => 'r',
+       'm' => 'k', 'k' => 'm',
+       'd' => 'h', 'h' => 'd',
+       'v' => 'b', 'b' => 'v',
+       's' => 's', 'w' => 'w', 'n' => 'n',
+       'u' => 'a'
+     }[base] || base
+  end
+
+  def reverse_complement(bases) when is_bitstring(bases) do
+    complement(bases)
+    |> String.reverse
+  end
+  def reverse_complement(bases) when is_list(bases) do
+    complement(bases)
+    |> Enum.reverse
   end
 
 end
--- test/bio/sequence/na_test.exs
+++ test/bio/sequence/na_test.exs
@@ -2,28 +2,17 @@ defmodule Bio.Sequence.NATest do
   use ExUnit.Case
   doctest Bio.Sequence.NA
 
-  test "default initialization" do
-    x = %Bio.Sequence.NA{}
-    assert x.seq == ""
+  # forward complementing
+  test "accepts a single character" do 
+    assert Bio.Sequence.NA.complement('c') == 'g'
   end
-  test "initialization with an argument" do
-    x = %Bio.Sequence.NA{seq: "ACTG"}
-    assert x.seq == "ACTG"
-  end
-
-  # reverse complementing
-  test "accepts a single character" do
-    assert Bio.Sequence.NA.reverse_complement("a") == "t"
+  test "accepts a single bitstring character" do
+    assert Bio.Sequence.NA.complement("a") == "t"
   end
   test "accepts a string" do
-    assert Bio.Sequence.NA.reverse_complement("actg") == "tgac"
-  end
-  test "accepts an NA struct" do
-    x = %Bio.Sequence.NA{seq: "ACTG"}
-    expected = %Bio.Sequence.NA{seq: "tgac"}
-    assert Bio.Sequence.NA.reverse_complement(x) == expected
+    assert Bio.Sequence.NA.complement("actg") == "tgac"
   end
   test "returns unmatched characters" do
-    assert Bio.Sequence.NA.reverse_complement("acXgt") == "tgXca"
+    assert Bio.Sequence.NA.complement("acXgt") == "tgxca"
   end
 end
